f81cbd294b5f2bcf8db3794516aa7938bc4e4750
cezarik8 <c_a_cretu@yahoo.com>
2014-10-26 19:55:07 +0200

    Initial commit

---
+++ src/tema/main.java
@@ -0,0 +1,16 @@
+package tema;
+
+/**
+ * Created by Cezar on 10/26/2014.
+ * @version 1.0
+ * @autor Cretu
+ */
+public class main {
+    public static void main(String [] args )
+
+    {
+        //primul proiect :)
+    }
+
+    }
+
